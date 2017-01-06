354a58dbd680056609d1d91d0a6cc3e6ec22ae4a
Vikramjeet Singh <viksingh@apple.com>
2017-01-05 16:59:17 -0800

    started neural network implementation

---
+++ AI_algos/Neural/Neural.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+let dataset: [[Double]] = [[2.7810836,2.550537003,0],
+               [1.465489372,2.362125076,0],
+               [3.396561688,4.400293529,0],
+               [1.38807019,1.850220317,0],
+               [3.06407232,3.005305973,0],
+               [7.627531214,2.759262235,1],
+               [5.332441248,2.088626775,1],
+               [6.922596716,1.77106367,1],
+               [8.675418651,-0.242068655,1],
+               [7.673756466,3.508563011,1]]
+
+struct Neuron {
+    var weights: [Double]
+    
+    init(_ inputs: Int) {
+        weights = (0..<inputs).map{ _ in drand48() }
+    }
+}
+
+struct Layer {
+    var neurons: [Neuron]
+    
+    init(inputs: Int, numberOfNeurons: Int) {
+        neurons = (0..<numberOfNeurons).map{ _ in Neuron(inputs) }
+    }
+}
+
+struct NeuralNetwork {
+    let outputLayer: Layer
+    let hiddenLayer: Layer
+    
+    init(inputs: Int, numOfHiddenNeurons: Int, numOfOutputNeurons: Int) {
+        hiddenLayer = Layer(inputs: inputs, numberOfNeurons: numOfHiddenNeurons)
+        outputLayer = Layer(inputs: inputs, numberOfNeurons: numOfOutputNeurons)
+    }
+}
+
+// test
+let nn = NeuralNetwork(inputs: 5, numOfHiddenNeurons: 1, numOfOutputNeurons: 2)
+print(nn)
